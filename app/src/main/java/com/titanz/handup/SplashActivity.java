1ffd06e07aab4ac2be7ddbdf3830b24fd684f2d9
Erik Antunes <erik.antunes@live.com>
2019-09-11 03:50:54 -0300

    splash pronto

---
+++ app/src/main/java/com/titanz/handup/SplashActivity.java
@@ -0,0 +1,54 @@
+package com.titanz.handup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        changeStatusBarColor();
+
+        irParaTelaPrincipal();
+
+    }
+
+    private void irParaTelaPrincipal() {
+        Handler handle = new Handler();
+        handle.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+
+    private void changeStatusBarColor() {
+
+        // LightBlueColor
+        String statusBarcolor = "#69e2ff";
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.parseColor(statusBarcolor));
+
+
+        }
+    }
+
+}
