806fefda7168cf7b4a25191d7075b51ccc25e414
Erik Antunes <erik.antunes@live.com>
2019-09-12 09:51:01 -0300

    Home

--- app/src/main/java/com/titanz/handup/MainActivity.java
+++ app/src/main/java/com/titanz/handup/MainActivity.java
@@ -1,5 +1,7 @@
 package com.titanz.handup;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -11,6 +13,8 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,10 +22,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        changeStatusBarColor();
+
+
+        FloatingActionButton fab = findViewById(R.id.fab_adicionar_card_id);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -51,5 +56,20 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void changeStatusBarColor() {
+
+        // DarkBlueColor
+        String statusBarcolor = "#0081cb";
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.parseColor(statusBarcolor));
+
+
+        }
     }
 }
