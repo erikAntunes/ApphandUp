260b7468131c5a0703760bc6b81a191c34bab87e
Erik Antunes <erik.antunes@live.com>
2019-09-13 11:45:46 -0300

    adapter pronto

--- app/src/main/java/com/titanz/handup/adapter/QuotesAdapter.java
+++ app/src/main/java/com/titanz/handup/adapter/QuotesAdapter.java
@@ -1,10 +1,14 @@
 package com.titanz.handup.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.titanz.handup.R;
 import com.titanz.handup.model.Quote;
 
 import java.util.ArrayList;
@@ -19,11 +23,19 @@ public class QuotesAdapter extends RecyclerView.Adapter<QuotesAdapter.ViewHolder
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.quote_celula,parent,false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+
+        viewHolder.nome.setText(quoteList.get(position).getNome());
+
+        viewHolder.oscilacao.setText(quoteList.get(position).getOscilacao());
+
+        viewHolder.valor.setText(quoteList.get(position).getValor());
 
     }
 
@@ -33,8 +45,18 @@ public class QuotesAdapter extends RecyclerView.Adapter<QuotesAdapter.ViewHolder
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView nome;
+        TextView oscilacao;
+        TextView valor;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            nome = itemView.findViewById(R.id.quote_nome_celula_id);
+            oscilacao = itemView.findViewById(R.id.quote_oscilacao_celula_id);
+            valor = itemView.findViewById(R.id.quote_valor_celula_id);
+
 
         }
     }
